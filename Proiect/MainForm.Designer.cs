3e064884b0faca4ce60ed76d82f2f9f3435b4621
miruna suliman <miruna.suliman@yahoo.com>
2023-05-06 18:31:21 +0300

    Add project files.

---
+++ Proiect/MainForm.Designer.cs
@@ -0,0 +1,353 @@
+﻿
+namespace Proiect
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.button1 = new System.Windows.Forms.Button();
+            this.aRe = new System.Windows.Forms.TextBox();
+            this.bIm = new System.Windows.Forms.TextBox();
+            this.bRe = new System.Windows.Forms.TextBox();
+            this.aIm = new System.Windows.Forms.TextBox();
+            this.cRe = new System.Windows.Forms.TextBox();
+            this.cIm = new System.Windows.Forms.TextBox();
+            this.dRe = new System.Windows.Forms.TextBox();
+            this.dIm = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.d = new System.Windows.Forms.Label();
+            this.c = new System.Windows.Forms.Label();
+            this.a = new System.Windows.Forms.Label();
+            this.b = new System.Windows.Forms.Label();
+            this.ec = new System.Windows.Forms.TextBox();
+            this.sol1 = new System.Windows.Forms.TextBox();
+            this.sol3 = new System.Windows.Forms.TextBox();
+            this.sol2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Thistle;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(690, 412);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 91);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Calculeaza";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // aRe
+            // 
+            this.aRe.BackColor = System.Drawing.Color.Lavender;
+            this.aRe.Font = new System.Drawing.Font("Century Schoolbook", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.aRe.ForeColor = System.Drawing.Color.Black;
+            this.aRe.Location = new System.Drawing.Point(83, 84);
+            this.aRe.Multiline = true;
+            this.aRe.Name = "aRe";
+            this.aRe.Size = new System.Drawing.Size(120, 44);
+            this.aRe.TabIndex = 3;
+            // 
+            // bIm
+            // 
+            this.bIm.BackColor = System.Drawing.Color.Lavender;
+            this.bIm.Font = new System.Drawing.Font("Century Schoolbook", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bIm.Location = new System.Drawing.Point(291, 134);
+            this.bIm.Multiline = true;
+            this.bIm.Name = "bIm";
+            this.bIm.Size = new System.Drawing.Size(120, 44);
+            this.bIm.TabIndex = 4;
+            // 
+            // bRe
+            // 
+            this.bRe.BackColor = System.Drawing.Color.Lavender;
+            this.bRe.Font = new System.Drawing.Font("Century Schoolbook", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bRe.Location = new System.Drawing.Point(83, 134);
+            this.bRe.Multiline = true;
+            this.bRe.Name = "bRe";
+            this.bRe.Size = new System.Drawing.Size(120, 44);
+            this.bRe.TabIndex = 5;
+            // 
+            // aIm
+            // 
+            this.aIm.BackColor = System.Drawing.Color.Lavender;
+            this.aIm.Font = new System.Drawing.Font("Century Schoolbook", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.aIm.Location = new System.Drawing.Point(291, 84);
+            this.aIm.Multiline = true;
+            this.aIm.Name = "aIm";
+            this.aIm.Size = new System.Drawing.Size(120, 44);
+            this.aIm.TabIndex = 6;
+            // 
+            // cRe
+            // 
+            this.cRe.BackColor = System.Drawing.Color.Lavender;
+            this.cRe.Font = new System.Drawing.Font("Century Schoolbook", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cRe.Location = new System.Drawing.Point(83, 184);
+            this.cRe.Multiline = true;
+            this.cRe.Name = "cRe";
+            this.cRe.Size = new System.Drawing.Size(120, 44);
+            this.cRe.TabIndex = 7;
+            // 
+            // cIm
+            // 
+            this.cIm.BackColor = System.Drawing.Color.Lavender;
+            this.cIm.Font = new System.Drawing.Font("Century Schoolbook", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cIm.Location = new System.Drawing.Point(291, 184);
+            this.cIm.Multiline = true;
+            this.cIm.Name = "cIm";
+            this.cIm.Size = new System.Drawing.Size(120, 44);
+            this.cIm.TabIndex = 8;
+            // 
+            // dRe
+            // 
+            this.dRe.BackColor = System.Drawing.Color.Lavender;
+            this.dRe.Font = new System.Drawing.Font("Century Schoolbook", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dRe.Location = new System.Drawing.Point(83, 234);
+            this.dRe.Multiline = true;
+            this.dRe.Name = "dRe";
+            this.dRe.Size = new System.Drawing.Size(120, 44);
+            this.dRe.TabIndex = 9;
+            // 
+            // dIm
+            // 
+            this.dIm.BackColor = System.Drawing.Color.Lavender;
+            this.dIm.Font = new System.Drawing.Font("Century Schoolbook", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dIm.Location = new System.Drawing.Point(291, 236);
+            this.dIm.Multiline = true;
+            this.dIm.Name = "dIm";
+            this.dIm.Size = new System.Drawing.Size(120, 44);
+            this.dIm.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(63, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 37);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Partea reala";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(252, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(202, 37);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Partea imaginara";
+            // 
+            // d
+            // 
+            this.d.AutoSize = true;
+            this.d.Font = new System.Drawing.Font("Lucida Bright", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.d.Location = new System.Drawing.Point(25, 236);
+            this.d.Name = "d";
+            this.d.Size = new System.Drawing.Size(35, 34);
+            this.d.TabIndex = 14;
+            this.d.Text = "d";
+            // 
+            // c
+            // 
+            this.c.AutoSize = true;
+            this.c.Font = new System.Drawing.Font("Lucida Bright", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c.Location = new System.Drawing.Point(25, 184);
+            this.c.Name = "c";
+            this.c.Size = new System.Drawing.Size(31, 34);
+            this.c.TabIndex = 15;
+            this.c.Text = "c";
+            // 
+            // a
+            // 
+            this.a.AutoSize = true;
+            this.a.Font = new System.Drawing.Font("Lucida Bright", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a.Location = new System.Drawing.Point(25, 86);
+            this.a.Name = "a";
+            this.a.Size = new System.Drawing.Size(32, 34);
+            this.a.TabIndex = 16;
+            this.a.Text = "a";
+            // 
+            // b
+            // 
+            this.b.AutoSize = true;
+            this.b.Font = new System.Drawing.Font("Lucida Bright", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b.Location = new System.Drawing.Point(25, 134);
+            this.b.Name = "b";
+            this.b.Size = new System.Drawing.Size(35, 34);
+            this.b.TabIndex = 17;
+            this.b.Text = "b";
+            // 
+            // ec
+            // 
+            this.ec.BackColor = System.Drawing.Color.Lavender;
+            this.ec.Font = new System.Drawing.Font("Century Schoolbook", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ec.Location = new System.Drawing.Point(40, 60);
+            this.ec.Multiline = true;
+            this.ec.Name = "ec";
+            this.ec.Size = new System.Drawing.Size(514, 47);
+            this.ec.TabIndex = 20;
+            // 
+            // sol1
+            // 
+            this.sol1.BackColor = System.Drawing.Color.Lavender;
+            this.sol1.Font = new System.Drawing.Font("Century Schoolbook", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.sol1.Location = new System.Drawing.Point(18, 72);
+            this.sol1.Multiline = true;
+            this.sol1.Name = "sol1";
+            this.sol1.Size = new System.Drawing.Size(370, 75);
+            this.sol1.TabIndex = 22;
+            // 
+            // sol3
+            // 
+            this.sol3.BackColor = System.Drawing.Color.Lavender;
+            this.sol3.Font = new System.Drawing.Font("Century Schoolbook", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.sol3.Location = new System.Drawing.Point(18, 234);
+            this.sol3.Multiline = true;
+            this.sol3.Name = "sol3";
+            this.sol3.Size = new System.Drawing.Size(370, 75);
+            this.sol3.TabIndex = 23;
+            // 
+            // sol2
+            // 
+            this.sol2.BackColor = System.Drawing.Color.Lavender;
+            this.sol2.Font = new System.Drawing.Font("Century Schoolbook", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.sol2.Location = new System.Drawing.Point(18, 153);
+            this.sol2.Multiline = true;
+            this.sol2.Name = "sol2";
+            this.sol2.Size = new System.Drawing.Size(370, 75);
+            this.sol2.TabIndex = 24;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ec);
+            this.groupBox1.Font = new System.Drawing.Font("Imprint MT Shadow", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 385);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(601, 144);
+            this.groupBox1.TabIndex = 25;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ecuatia";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.aRe);
+            this.groupBox2.Controls.Add(this.bRe);
+            this.groupBox2.Controls.Add(this.d);
+            this.groupBox2.Controls.Add(this.c);
+            this.groupBox2.Controls.Add(this.b);
+            this.groupBox2.Controls.Add(this.cRe);
+            this.groupBox2.Controls.Add(this.a);
+            this.groupBox2.Controls.Add(this.dRe);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.aIm);
+            this.groupBox2.Controls.Add(this.bIm);
+            this.groupBox2.Controls.Add(this.cIm);
+            this.groupBox2.Controls.Add(this.dIm);
+            this.groupBox2.Font = new System.Drawing.Font("Imprint MT Shadow", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(489, 351);
+            this.groupBox2.TabIndex = 26;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Coeficientii ecuatiei";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.sol1);
+            this.groupBox3.Controls.Add(this.sol2);
+            this.groupBox3.Controls.Add(this.sol3);
+            this.groupBox3.Font = new System.Drawing.Font("Imprint MT Shadow", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(519, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(409, 351);
+            this.groupBox3.TabIndex = 27;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Solutiile ecuatiei";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(949, 589);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ecuatia de gradul 3";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox aRe;
+        private System.Windows.Forms.TextBox bIm;
+        private System.Windows.Forms.TextBox bRe;
+        private System.Windows.Forms.TextBox aIm;
+        private System.Windows.Forms.TextBox cRe;
+        private System.Windows.Forms.TextBox cIm;
+        private System.Windows.Forms.TextBox dRe;
+        private System.Windows.Forms.TextBox dIm;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label d;
+        private System.Windows.Forms.Label c;
+        private System.Windows.Forms.Label a;
+        private System.Windows.Forms.Label b;
+        private System.Windows.Forms.TextBox ec;
+        private System.Windows.Forms.TextBox sol1;
+        private System.Windows.Forms.TextBox sol3;
+        private System.Windows.Forms.TextBox sol2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ Proiect/MainForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Proiect
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+			Complex p, q, delta, t, alpha, j;
+
+			Complex a = Complex.setReIm(0, 0);
+			Complex b = Complex.setReIm(0, 0);
+			Complex c = Complex.setReIm(0, 0);
+			Complex d = Complex.setReIm(0, 0);
+
+			a.Re = Convert.ToInt32(aRe.Text);
+			a.Im= Convert.ToInt32(aIm.Text);
+
+			b.Re = Convert.ToInt32(bRe.Text);
+			b.Im = Convert.ToInt32(bIm.Text);
+
+			c.Re = Convert.ToInt32(cRe.Text);
+			c.Im = Convert.ToInt32(cIm.Text);
+
+			d.Re = Convert.ToInt32(dRe.Text);
+			d.Im = Convert.ToInt32(dIm.Text);
+
+			ec.Text = "("+a+")" + "x^3+" + "("+ b + ")" + "x^2+" + "("+ c + ")" + "x+" + "("+ d+ ")" + "=0";
+
+            Complex z1 = Complex.setReIm(0, 1);
+			p = (-(b * b) + 3 * a * c) / (3 * a * a);
+			q = (2 * b * b * b + 27 * d * a * a - 9 * a * b * c) / (27 * a * a * a);
+			delta = q * q + (4 * p * p * p / 27);
+			t = (-q + delta.sqrt(2)) / 2;
+			alpha = t.sqrt(3);
+			j = (-1 + z1 * Math.Sqrt(3)) / 2;
+			Complex h = b / (3 * a);
+
+			if (delta != 0)
+			{
+				sol1.Text = j.pow(0) * alpha - j.pow(2 * 0) * (p / (3 * alpha)) - h + " ";
+				sol2.Text = j.pow(1) * alpha - j.pow(2 * 1) * (p / (3 * alpha)) - h + " ";
+				sol3.Text = j.pow(2) * alpha - j.pow(2 * 2) * (p / (3 * alpha)) - h + " ";
+			}
+			else
+			{
+				alpha = (-p / 3).sqrt(2);
+				sol1.Text = 2 * alpha - b / 3 * a + " ";
+				sol2.Text=-alpha - b / 3 * a+" ";
+			}
+
+
+
+		}
+    }
+}
